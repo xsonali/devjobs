d673c46b3e9170ab36972e2745474ec86bbaee37
xsonali <villshona@gmail.com>
2025-07-08 09:05:09 +1000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,6 +5,6 @@ terraform {
     resource_group_name  = "REPLACE_ME"          # Your actual resource group name
     storage_account_name = "REPLACE_ME"          # Your Azure Storage Account name
     container_name       = "REPLACE_ME"               # Blob container name for state files
-    key                  = "dREPLACE_ME" # State file name
+    key                  = "REPLACE_ME" # State file name
   }
 }
